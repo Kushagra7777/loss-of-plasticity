5d27dc5b7574347b7fe4af92e3b97f95ac305191
Kushagra  Srivastav <srivastavkushagra777@gmail.com>
2025-01-11 14:56:45 +0530

    simplified permutation script
    A simplified script independent of any model. It generates permutated MNIST data and saves it.

---
+++ lop/permuted_mnist/permutation_script.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+import pickle
+
+
+with open('data/mnist_', 'rb+') as f:
+    x, y, _, _ = pickle.load(f)
+
+
+num_tasks = 800
+examples_per_task = 60000
+input_size = 784
+
+
+for task_idx in range(num_tasks):
+    # pixel-wise permutation
+    pixel_permutation = np.random.permutation(input_size)  # Random permutation 
+    permuted_x = x[:, pixel_permutation]  # applying permutation to all images
+
+    # shuffling the images
+    data_permutation = np.random.permutation(examples_per_task)  
+    permuted_x, permuted_y = permuted_x[data_permutation], y[data_permutation]
+
+    permuted_x = permuted_x / 255.0 #normalizing
+
+    # Saving
+    task_filename = f'data/permutated_data/permuted_task_{task_idx + 1}.pkl'  
+    with open(task_filename, 'wb') as task_file:
+        pickle.dump([permuted_x, permuted_y], task_file)  # Saving images and corresponding labels
+
+    print(f"Task {task_idx + 1} saved successfully!")
+
+print("All tasks have been generated and saved.")
